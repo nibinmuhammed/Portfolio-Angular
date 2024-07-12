e047bf85a4076dae0977e70ad697408bfee63aa4
nibinmuhammed <nibin.muhammed@vensure.com>
2024-07-12 09:43:47 +0530

    navigation manu added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+import { routes } from './app.routes';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLinkActive, RouterLink, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { CertificationsComponent } from './certifications/certifications.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch:"full" },
+    { path: 'home', component: HomeComponent },
+    { path: 'about-me', component: AboutMeComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'certifications', component: CertificationsComponent },
+    { path: 'contact-me', component: ContactMeComponent },
+    { path: '**', redirectTo: '/home', pathMatch:"full" }
+];
